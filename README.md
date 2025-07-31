0f08ac026cae3e6c8df4fa9fff592a9ed697fe46
Deiner Rodrigues de Souza <deiner.souza@win.ufu.br>
2025-07-30 21:22:02 -0300

    bug to generate csv fixed

--- README.md
+++
@@ -1,1 +0,0 @@
-"Projeto com cunho educativo mesclando an lise financeira de mercado de Data Science" 
--- generate_action_csv/infomoney/scraping.py
+++ generate_action_csv/infomoney/scraping.py
@@ -1,3 +1,4 @@
+import sys
 import requests
 import pandas as pd
 import time
@@ -15,52 +16,58 @@ class Scraping():
             "Accept": "application/json"
         } 
         self._delay = np.random.uniform(1,2)
+        self._base_dir = ""
 
     def run(self):
-        try:
-            self._make_indicators_data()
-        except Exception as e:
-            day = datetime.now().strftime("%Y-%m-%d")
-            hours = datetime.now().strftime("%H:%M:%S")            
-            with open(self._log_path(f"{day}log.txt"), "a", encoding="utf-8") as log:
-                log.write(f"\n{hours}ERROR:{str(e)}")
 
-
-
-
-    def _make_indicators_data(self):
         print("\nStart first operation completed\n")
         codes_actions = self._extract_codes_actions()
         self._list_is_valid(codes_actions)
-        os.system("cls")
+        os.system("cls")        
         print("\nOperation completed\n")
-        time.sleep(2)
-        print("\nStart second operation completed\n")
+        time.sleep(3)
 
-        table_data = {}
+        os.system("cls")
+        print("\nStart second operation completed\n")
+        self._try_make_indicators_data(codes_actions)
+        
+
+    def _try_make_indicators_data(self,codes_actions):
+        try:
+            self._make_indicators_data(codes_actions)
+        except Exception as e:
+            day = datetime.now().strftime("%Y-%m-%d")
+            hours = datetime.now().strftime("%H:%M:%S")            
+            with open(self._path(f"./{day}log.txt","logs"), "a", encoding="utf-8") as log:
+                log.write(f"\n{hours}ERROR:{str(e)}")
+                print("Sorry something went wrong :/")
+
+
+    def _make_indicators_data(self,codes_actions):
+        table_data = []
         count = 0
         for code_action in codes_actions:
             count+=1
-            data = self._try_extract(code_action,count)
-            table_data = table_data | data
+            data = self._try_extract_indicators_of(code_action,count)
+            table_data.append(data)
             time.sleep(self._delay)
-                        
+                    
         df = pd.DataFrame(table_data)
-        df.to_csv(self._csv_path("actions_indicators.csv"))
-        os.system("cls")
+        df.to_csv(self._path("./actions_indicators.csv","extraction"))
+        os.system("cls" if os.name == "nt" else "clear") 
         print("\nScraping finished\n")
 
 
-    def _try_extract(self,code_action,count):
+    def _try_extract_indicators_of(self,code_action,count):
         try:
-            data = self._extract_indicators(code_action)
+            data = self._extract_indicators_of(code_action)
             print(f"{count}° - operation completed - {code_action}")
             return data
                   
         except Exception as e:
             day = datetime.now().strftime("%Y-%m-%d")
             hours = datetime.now().strftime("%H:%M:%S")            
-            with open(self._log_path(f"{day}log.txt"), "a", encoding="utf-8") as log:
+            with open(self._path(f"./{day}log.txt","logs"), "a", encoding="utf-8") as log:
                 log.write(f"\n{hours} - {code_action} - [Extraction] ERROR:{str(e)}")
                 print(f"{count}° - operation failure - {code_action}")
             return {}
@@ -68,7 +75,7 @@ class Scraping():
 
 
 
-    def _extract_indicators(self,code_action):
+    def _extract_indicators_of(self,code_action):
         url = f"http://www.infomoney.com.br/{code_action}"
         response = requests.get(url,headers=self._headers)
         soup  = BeautifulSoup(response.text, "html.parser")
@@ -99,7 +106,7 @@ class Scraping():
             print(f"Pagina: {page} - \n{[stockcode["StockCode"] for stockcode in monitor]}")
             if len(monitor) == 0:
                 break
-            data.extend(resp.json()["Data"])           
+            data.extend(resp.json()["Data"])          
             page += 1
             time.sleep(self._delay)
         
@@ -116,20 +123,17 @@ class Scraping():
 
 
 
-    def _log_path(self,name):
-        base_dir = os.path.dirname(os.path.abspath(__file__))      
-        log_dir = os.path.join(base_dir, 'logs')        
-        os.makedirs(log_dir, exist_ok=True)        
-        log_file = os.path.join(log_dir, name)
-        return log_file
-    
-    def _csv_path(self,name):
-        base_dir = os.path.dirname(os.path.abspath(__file__))      
-        log_dir = os.path.join(base_dir, 'extraction')        
-        os.makedirs(log_dir, exist_ok=True)        
-        log_file = os.path.join(log_dir, name)
-        return log_file
+    def _path(self, name, dir):
+        if getattr(sys, "frozen", False):
+            self._base_path = os.path.dirname(sys.executable)
+        else:
+            self._base_path = os.path.dirname(os.path.abspath(__file__))
 
+        path_final = os.path.join(self._base_path, dir)
+        os.makedirs(path_final, exist_ok=True)
+        return os.path.join(path_final, name)
+    
+  
 
 
 
